c3d8f0b4d849fee939339f6b5153f5ede871604e
MinRK <benjaminrk@gmail.com>
2014-09-04 14:51:17 -0700

    fix relative import of _dummy

--- appnope/__init__.py
+++ appnope/__init__.py
@@ -5,7 +5,7 @@ import platform
 from distutils.version import LooseVersion as V
 
 if sys.platform != "darwin" or V(platform.mac_ver()[0]) < V("10.9"):
-    from _dummy import *
+    from ._dummy import *
 else:
     from ._nope import *
 
