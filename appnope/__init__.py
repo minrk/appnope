c126265110be8fb0d57071ca6a0e4c4c0859253e
Min RK <benjaminrk@gmail.com>
2024-02-06 09:38:30 +0100

    fix regex escape
    aviods DeprecationWarning

--- appnope/__init__.py
+++ appnope/__init__.py
@@ -5,7 +5,7 @@ import sys
 import platform
 
 def _v(version_s):
-    return tuple(int(s) for s in re.findall("\d+", version_s))
+    return tuple(int(s) for s in re.findall(r"\d+", version_s))
 
 if sys.platform != "darwin" or _v(platform.mac_ver()[0]) < _v("10.9"):
     from ._dummy import *
